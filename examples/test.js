559b34c25470b4a2b002db3cd3b485ab39fea8cb
sswsdsn <hrblhf08@gmail.com>
2018-10-25 15:18:43 +0800

    add blockchain methods

--- examples/test.js
+++ examples/test.js
@@ -4,4 +4,11 @@ const {Account, IOST, HTTPProvider} = require('../index');
 
 const iost = new IOST(new HTTPProvider('http://54.95.136.154:30001'));
 
-iost.net.getNetInfo().then(console.log);
+// net
+// iost.net.getNetInfo().then(console.log);
+
+// chainInfo
+// iost.blockchain.getChainInfo().then(console.log);
+
+// getBlockByNum
+// iost.blockchain.getBlockByNum(1234, 1).then(console.log);
