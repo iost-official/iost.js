346b83f73f07f6993c668e34b7d3cf85891304ef
He Peijian <glisxx6@gmail.com>
2018-11-29 18:16:45 +0800

    fix username

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ const {Account, RPC, HTTPProvider} = require('./index');
 const rpc = new RPC(new HTTPProvider('http://192.168.1.144:20001'));
 
 rpc.blockchain.getContractStorage("Contract3dUfteTf9qR94D7f3xFUMoxJ4iErtnSjAwG6HuAVNWUs",
-    "admin",
+    "IOST4wQ6HPkSrtDRYi2TGkyMJZAB3em26fx79qR3UJC7fcxpL87wTn",
     "w0").then(function (s) {
     	document.write(s.jsonStr)
     });
