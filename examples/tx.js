73080cfdc5a319392534ed837d0e8caa3da1788f
He Peijian <glisxx6@gmail.com>
2018-11-30 16:55:55 +0800

    fix typo

--- examples/tx.js
+++ examples/tx.js
@@ -9,7 +9,7 @@ let iost = new IOST({ // 如果不设置则使用default配置来发交易
 }, new IOST.HTTPProvider('http://localhost:30001'));
 
 let account = "abc";
-let kp = new IOST.KeyPair(bs58.decode('1rANSfcRzr4HkhbUFZ7L1Zp69JZZHiDDq5v7dNSbbEqeU4jxy3fszV4HGiaLQEyqVpS1dKT9g7zCVRxBVzuiUzB');
+let kp = new IOST.KeyPair(bs58.decode('1rANSfcRzr4HkhbUFZ7L1Zp69JZZHiDDq5v7dNSbbEqeU4jxy3fszV4HGiaLQEyqVpS1dKT9g7zCVRxBVzuiUzB'));
 
 iost.setPublisher(account, kp);
 
