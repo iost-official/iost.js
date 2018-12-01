79daaa404a14a657c1715dc47c7aa3c356c59d00
He Peijian <glisxx6@gmail.com>
2018-12-01 16:27:37 +0800

    export tx

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const RPC = require('./lib/rpc');
 const HTTPProvider = require('./lib/provider/HTTPProvider');
 const IOST = require('./iost/iost');
 const KeyPair = require('./lib/crypto/key_pair');
+const {Tx} = require('./lib/structs');
 
 module.exports = {
 	IOST: IOST,
     RPC: RPC,
     HTTPProvider: HTTPProvider,
-    KeyPair: KeyPair
+    KeyPair: KeyPair,
+    Tx : Tx,
 };
