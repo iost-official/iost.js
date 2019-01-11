ca0bec8c7c288d15ae1e7cd67a1a8751ddb85013
Zhou Xiao <616702857@qq.com>
2019-01-11 19:48:44 +0800

    Update gas_test.js

--- examples/gas_test.js
+++ examples/gas_test.js
@@ -29,7 +29,7 @@ const iost = new IOST.IOST({ // will use default setting if not set
     gasLimit: 1000000,
     delay:0,
     expiration: 90,
-    defaultLimit: "1000"
+    defaultLimit: "10000"
 });
 const rpc = new IOST.RPC(new IOST.HTTPProvider('http://127.0.0.1:30001'));
 
@@ -39,7 +39,8 @@ const kp = new KeyPair(bs58.decode('2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEW
 account.addKeyPair(kp, "owner");
 account.addKeyPair(kp, "active");
 
-const account_producer = new IOST.Account("producer00001");
+let producerid = "producer01"
+const account_producer = new IOST.Account(producerid);
 const kp_producer = new KeyPair(bs58.decode('1rANSfcRzr4HkhbUFZ7L1Zp69JZZHiDDq5v7dNSbbEqeU4jxy3fszV4HGiaLQEyqVpS1dKT9g7zCVRxBVzuiUzB'));
 account_producer.addKeyPair(kp_producer, "owner");
 account_producer.addKeyPair(kp_producer, "active");
@@ -53,6 +54,8 @@ let userPrefix = Date.now().toString();
 userPrefix = "u" + userPrefix.substr(userPrefix.length - 8);
 let tokenSym = Date.now().toString();
 tokenSym = "t" + tokenSym.substr(tokenSym.length - 4);
+let myid = userPrefix + "my";
+let otherid = userPrefix + "ot";
 
 delay().then(async function () {
     let accountInfo = await rpc.blockchain.getAccountInfo("admin", false);
@@ -95,7 +98,7 @@ delay().then(async function () {
 }).then(function () {
     const kp = KeyPair.newKeyPair();
     const tx = iost.newAccount(
-        "myidid",
+        myid,
         "admin",
         kp.id,
         kp.id,
@@ -107,7 +110,7 @@ delay().then(async function () {
     handler
         .onSuccess(function (response) {
             console.log("Success... tx, receipt: "+ JSON.stringify(response));
-            accountList[0] = new IOST.Account("myidid");
+            accountList[0] = new IOST.Account(myid);
             accountList[0].addKeyPair(kp, "owner");
             accountList[0].addKeyPair(kp, "active");
         })
@@ -115,16 +118,16 @@ delay().then(async function () {
         .listen(1000, 5);
     return checkHandler(handler)
 }).then(async function () {
-    let accountInfo = await rpc.blockchain.getAccountInfo("myidid", false);
+    let accountInfo = await rpc.blockchain.getAccountInfo(myid, false);
     console.log(accountInfo);
     let limit1 = accountInfo.gas_info.limit;
-    const tx = iost.callABI("gas.iost", "pledge", ["admin", "myidid", "5"]);
+    const tx = iost.callABI("gas.iost", "pledge", ["admin", myid, "5"]);
     account.signTx(tx);
     const handler = new IOST.TxHandler(tx, rpc);
     handler
         .onSuccess(async function (response) {
             console.log("Success... tx, receipt: " + JSON.stringify(response));
-            let accountInfo = await rpc.blockchain.getAccountInfo("myidid", false);
+            let accountInfo = await rpc.blockchain.getAccountInfo(myid, false);
             console.log(accountInfo);
             let limit2 = accountInfo.gas_info.limit;
             assert.equal(limit1, limit2 - 300000 * 5)
@@ -133,16 +136,16 @@ delay().then(async function () {
         .listen(1000, 10);
     return checkHandler(handler)
 }).then(async function () {
-    let accountInfo = await rpc.blockchain.getAccountInfo("myidid", false);
+    let accountInfo = await rpc.blockchain.getAccountInfo(myid, false);
     console.log(accountInfo);
     let limit1 = accountInfo.gas_info.limit;
-    const tx = iost.callABI("gas.iost", "unpledge", ["admin", "myidid", "5"]);
+    const tx = iost.callABI("gas.iost", "unpledge", ["admin", myid, "5"]);
     account.signTx(tx);
     const handler = new IOST.TxHandler(tx, rpc);
     handler
         .onSuccess(async function (response) {
             console.log("Success... tx, receipt: " + JSON.stringify(response));
-            let accountInfo = await rpc.blockchain.getAccountInfo("myidid", false);
+            let accountInfo = await rpc.blockchain.getAccountInfo(myid, false);
             console.log(accountInfo);
             let limit2 = accountInfo.gas_info.limit;
             assert.equal(limit1, limit2 + 300000 * 5)
@@ -151,20 +154,33 @@ delay().then(async function () {
         .listen(1000, 10);
     return checkHandler(handler)
 }).then(async function () {
-    let accountInfo = await rpc.blockchain.getAccountInfo("producer00001", false);
+    const tx = iost.callABI("token.iost", "transfer", ["iost", "admin", producerid, "10000", ""]);
+    account.signTx(tx);
+    const handler = new IOST.TxHandler(tx, rpc);
+    handler
+        .onSuccess(async function (response) {
+            console.log("Success... tx, receipt: " + JSON.stringify(response));
+            let accountInfo = await rpc.blockchain.getAccountInfo(producerid, false);
+            assert.equal(accountInfo.balance, 10000)
+        })
+        .send()
+        .listen(1000, 10);
+    return checkHandler(handler)
+}).then(async function () {
+    let accountInfo = await rpc.blockchain.getAccountInfo(producerid, false);
     console.log(accountInfo);
     let balance1 = accountInfo.balance
     const kp = KeyPair.newKeyPair();
-    const tx = iost.callABI("auth.iost", "SignUp", ["otherid", kp.id, kp.id]);
+    const tx = iost.callABI("auth.iost", "signUp", [otherid, kp.id, kp.id]);
     account_producer.signTx(tx);
     const handler = new IOST.TxHandler(tx, rpc);
     handler
         .onSuccess(async function (response) {
             console.log("Success... tx, receipt: "+ JSON.stringify(response));
-            accountList[1] = new IOST.Account("otherid");
+            accountList[1] = new IOST.Account(otherid);
             accountList[1].addKeyPair(kp, "owner");
             accountList[1].addKeyPair(kp, "active");
-            let accountInfo = await rpc.blockchain.getAccountInfo("producer00001", false);
+            let accountInfo = await rpc.blockchain.getAccountInfo(producerid, false);
             let balance2 = accountInfo.balance
             assert.equal(balance1, balance2 + 7)
         })
@@ -172,7 +188,7 @@ delay().then(async function () {
         .listen(1000, 5);
     return checkHandler(handler)
 }).then(async function () {
-    const tx = iost.callABI("gas.iost", "pledge", ["producer00001", "otherid", "10"]);
+    const tx = iost.callABI("gas.iost", "pledge", [producerid, otherid, "10"]);
     account_producer.signTx(tx);
     const handler = new IOST.TxHandler(tx, rpc);
     handler
@@ -183,15 +199,15 @@ delay().then(async function () {
         .listen(1000, 5);
     return checkHandler(handler)
 }).then(async function () {
-    let accountInfo = await rpc.blockchain.getAccountInfo("otherid", false);
+    let accountInfo = await rpc.blockchain.getAccountInfo(otherid, false);
     console.log(accountInfo);
-    const tx = iost.callABI("token.iost", "transfer", ["iost", "otherid", "producer00001", "1", ""]);
+    const tx = iost.callABI("token.iost", "transfer", ["iost", otherid, producerid, "1", ""]);
     accountList[1].signTx(tx);
     const handler = new IOST.TxHandler(tx, rpc);
     handler
         .onFailed(async function (response) {
             console.log("Failed... tx, receipt: "+ JSON.stringify(response));
-            let accountInfo = await rpc.blockchain.getAccountInfo("producer00001", false);
+            let accountInfo = await rpc.blockchain.getAccountInfo(producerid, false);
             console.log(accountInfo);
             assert.equal(accountInfo.gas_info.transferable_gas, response.gas_usage*1.0/10)
         })
@@ -199,38 +215,38 @@ delay().then(async function () {
         .listen(1000, 5);
     return delay(10000)
 }).then(async function () {
-    let accountInfo1 = await rpc.blockchain.getAccountInfo("producer00001", false);
+    let accountInfo1 = await rpc.blockchain.getAccountInfo(producerid, false);
     console.log(accountInfo1);
-    let accountInfo2 = await rpc.blockchain.getAccountInfo("otherid", false);
+    let accountInfo2 = await rpc.blockchain.getAccountInfo(otherid, false);
     console.log(accountInfo2);
-    const tx = iost.callABI("gas.iost", "transfer", ["producer00001", "otherid","100"]);
+    const tx = iost.callABI("gas.iost", "transfer", [producerid, otherid,"100"]);
     account_producer.signTx(tx);
     const handler = new IOST.TxHandler(tx, rpc);
     handler
         .onSuccess(async function (response) {
             console.log("Success... tx, receipt: "+ JSON.stringify(response));
-            let accountInfo3 = await rpc.blockchain.getAccountInfo("producer00001", false);
+            let accountInfo3 = await rpc.blockchain.getAccountInfo(producerid, false);
             console.log(accountInfo3);
-            let accountInfo4 = await rpc.blockchain.getAccountInfo("otherid", false);
+            let accountInfo4 = await rpc.blockchain.getAccountInfo(otherid, false);
             console.log(accountInfo4);
         })
         .send()
         .listen(1000, 5);
     return checkHandler(handler)
 }).then(async function () {
-    let accountInfo1 = await rpc.blockchain.getAccountInfo("producer00001", false);
+    let accountInfo1 = await rpc.blockchain.getAccountInfo(producerid, false);
     console.log(accountInfo1);
-    let accountInfo2 = await rpc.blockchain.getAccountInfo("otherid", false);
+    let accountInfo2 = await rpc.blockchain.getAccountInfo(otherid, false);
     console.log(accountInfo2);
-    const tx = iost.callABI("gas.iost", "transfer", ["otherid", "producer00001","100"]);
+    const tx = iost.callABI("gas.iost", "transfer", [otherid, producerid,"100"]);
     accountList[1].signTx(tx);
     const handler = new IOST.TxHandler(tx, rpc);
     handler
         .onFailed(async function (response) {
             console.log("Failed... tx, receipt: "+ JSON.stringify(response));
-            let accountInfo3 = await rpc.blockchain.getAccountInfo("producer00001", false);
+            let accountInfo3 = await rpc.blockchain.getAccountInfo(producerid, false);
             console.log(accountInfo3);
-            let accountInfo4 = await rpc.blockchain.getAccountInfo("otherid", false);
+            let accountInfo4 = await rpc.blockchain.getAccountInfo(otherid, false);
             console.log(accountInfo4);
         })
         .send()
