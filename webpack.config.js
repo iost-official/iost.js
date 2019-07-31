97a0b3a4f3fac3dd7a96c58b5342aa706dc2b87b
tonokim <tonokim@qq.com>
2019-07-31 16:39:14 +0800

    nodejs

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,73 @@
-var path = require('path');
+const path = require('path');
 
-module.exports = {
+const baseConfig = {
   mode: 'production',
   devtool: false,
   entry: {
     iost : path.resolve(__dirname, './index.js')
   },
+}
+const serverConfig = {
+  ...baseConfig,
+  target: 'node',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].node.js',
     library: 'IOST',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
       { 
         test: /\.js$/, 
-        loader: 'babel-loader'
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                ['@babel/env', {
+                    targets: {
+                        node: 6
+                    }
+                }]
+              ],
+              plugins: [
+                "@babel/plugin-transform-async-to-generator"
+              ],
+            }
+          },
+        ]
       },
     ]
   },
-};
+}
+
+const clientConfig = {
+  ...baseConfig,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].min.js',
+    library: 'IOST',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              plugins: [
+                "@babel/plugin-transform-async-to-generator"
+              ],
+            }
+          },
+        ]
+      },
+    ]
+  },
+}
+
+module.exports = [ serverConfig, clientConfig ]
